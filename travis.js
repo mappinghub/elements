bd9611e578990b41a0e1c21808177b04562007d2
Daniel Taub <dmtaub@gmail.com>
2017-04-03 13:40:14 -0400

    adding package.json, travis.yml, script (untested)

---
+++ travis.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const shell = require('shelljs'),
+  path = require('path'),
+  got = require('got');
+
+
+console.log(`Fetching Git commit hash...`);
+
+const gitCommitRet = shell.exec('git rev-parse HEAD', {
+  cwd: path.join(__dirname, '..')
+});
+
+if (0 !== gitCommitRet.code) {
+  console.error('Error getting git commit hash');
+
+  process.exit(-1);
+}
+
+const gitCommitHash = gitCommitRet.stdout.trim();
+
+console.log(`Git commit: ${gitCommitHash}`);
+
+console.log('Calling Travis...');
+
+
+got.post(`https://api.travis-ci.org/repo/mappinghub/views`, {
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "Travis-API-Version": "3",
+    "Authorization": `token ${process.env.TRAVIS_API_TOKEN}`,
+  },
+  body: JSON.stringify({
+    request: {
+      message: `Trigger build at mappinghub/views commit: ${gitCommitHash}`,
+      branch: 'source',
+    },
+  }),
+})
+.then(() => {
+  console.log("Triggered build of mappinghub/views");
+})
+.catch((err) => {
+  console.error(err);
+
+  process.exit(-1);
+});
